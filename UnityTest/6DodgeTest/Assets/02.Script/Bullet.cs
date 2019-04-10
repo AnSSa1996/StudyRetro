3800a93f41ac3712bce5174f9cafbc624e381e94
sanghun1996 <skfro2013@naver.com>
2019-04-10 22:11:47 +0900

    레트로 유니티

---
+++ UnityTest/6DodgeTest/Assets/02.Script/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8.0f;
+    private Rigidbody bulletRigidbody;
+    // Use this for initialization
+    void Start()
+    {
+        bulletRigidbody = transform.GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = transform.forward * speed;
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+            if(playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+}
---
+++ UnityTest/6DodgeTest/Assets/02.Script/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime;
+    private bool isGameover;
+	// Use this for initialization
+	void Start () {
+        surviveTime = 0;
+        isGameover = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isGameover)
+        {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time: " + (int)surviveTime;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+	}
+
+    public void EndGame()
+    {
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+        isGameover = true;
+        gameoverText.SetActive(true);
+
+        if(surviveTime > bestTime)
+        {
+            bestTime = surviveTime;
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+        }
+
+        recordText.text = "Best Time: " + (int)bestTime;
+    }
+}
